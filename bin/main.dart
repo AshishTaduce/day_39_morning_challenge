636efe4a4994a513719c3225f3979102efea6b9d
Ashish <ashishteddy2@gmail.com>
2019-11-27 16:38:58 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -4,5 +4,35 @@
 // input: [[3,4],[1,2],[]]
 // output: [[[1,3,4],[2],[]], [[3,4],[2],[1]], [[4],[1,2],[3]]]
 
+List<List<List<int>>> possibleMoves(List<List<int>> input){
+  List<List<int>> copy=[];
+  List<List<List<int>>> result = [];
+//  for (List x in input){
+//    List temp = x;
+    copy.addAll(input);
+//  }
+
+    print('$input = input  || $copy = copy');
+    ///TODO : Logic to be created here
+    for(int i = 0; i< input.length; i++){
+      for(int j = 0; j< input.length; j++){
+        if(validMove(input, i, j)){
+          copy[j].insert(0, copy[i][0]);
+          copy[i].remove(copy[i][0]);
+          result.add(copy);
+        }
+      }
+    }
+    print('after func $input = input  || $copy = copy');
+    return result;
+  }
+
+
+
+bool validMove(List<List> input, int from, int to){
+  return(input[to].isEmpty || input[to].first > input[from].first);
+}
+
 main() {
+  print(possibleMoves([[3,4],[1,2],[]]));
 }
